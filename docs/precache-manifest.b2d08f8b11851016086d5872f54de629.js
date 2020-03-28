135c9d771e50381a1deedea2c51a137e4fe27931
r <r>
2020-03-28 19:48:42 +0300

    launch website on github

---
+++ docs/precache-manifest.b2d08f8b11851016086d5872f54de629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbedcd546ecacc58c2bb218f71955fcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d59ee3cd751dd6acee0",
+    "url": "/static/css/main.4283cad9.chunk.css"
+  },
+  {
+    "revision": "af8874e9bde949e10e49",
+    "url": "/static/js/2.f8565f67.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.f8565f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d59ee3cd751dd6acee0",
+    "url": "/static/js/main.2d6017fd.chunk.js"
+  },
+  {
+    "revision": "a97f54ed1fb5c0d7c337",
+    "url": "/static/js/runtime-main.2eb66568.js"
+  }
+]);
